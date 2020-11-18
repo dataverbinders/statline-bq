46030500abe9feee2c4582bbaecf6ffd68730f7e
amitgal <amitgalmail@gmail.com>
2020-11-18 12:27:41 +0100

    convert config from dict to named tuple

--- statline_bq/cli.py
+++ statline_bq/cli.py
@@ -11,26 +11,21 @@ def upload_datasets():
     """
     This CLI uploads datasets from CBS to Google Cloud Platform.
 
-    To run it, you must first have a GCP account, to which a GCS Project and a GCS Bucket
-    are connected. Additionally, you must hold the proper IAM (permissions) settings enabled
-    On this project.
+    To run it, you must first have a GCP account, to which a GCS Project and a
+    GCS Bucket are connected. Additionally, you must hold the proper IAM
+    (permissions) settings enabled on this project.
 
-    The GCP settings, along with a list of the datasets you wish to upload should be entered
-    into `config.toml`.
+    The GCP settings, along with a list of the datasets you wish to upload
+    should be manually written into `config.toml`.
 
     For further information, see the documentaion "????"
     """
-    config_file = Path("./statline_bq/config.toml")
-    with open(config_file) as conf:
-        config = toml.load(conf)
 
-    datasets = config["datasets"]["datasets"]
-    gcp = config["gcp"]
-
-    click.echo(f"The following datasets will be uploaded:")
-    for dataset in datasets:
+    config_path = Path("./config.toml")
+    config = commands.parse_config_toml(config_path)
+    click.echo("The following datasets will be downloaded from CBS and uploaded:")
+    for dataset in config.datasets:
         click.echo(f"{dataset}")
-    for dataset in config["datasets"]["datasets"]:
-        commands.cbs_odata_to_gcs(dataset, gcp=gcp)
-        # commands.cbs_odata(dataset, )
+    for dataset in config.datasets:
+        commands.cbs_odata_to_gcs(dataset, gcp=config.gcp)
     # click.echo(dataset)
