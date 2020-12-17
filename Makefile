9438c76b6fd2e9fd9741e095c50877d793786c4d
Amit Gal <amitgalmail@gmail.com>
2020-12-17 12:11:42 +0100

    add makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+pdoc: ## generate documentation with pdoc3
+	pdoc --html --force --output-dir docs --template-dir docs/pdoc_templates statline_bq
--- make_doc.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-if __name__ == "__main__":
-    subprocess.run(
-        # "pwd",
-        "pdoc --html --output-dir docs --template-dir docs/pdoc_templates statline_bq",
-        shell=True,
-    )
