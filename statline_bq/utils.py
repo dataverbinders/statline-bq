26f118afb147e56c1d1e73d59bfbcc7a0ec80810
Amit Gal <amitgalmail@gmail.com>
2021-01-08 11:46:23 +0200

    convert third_party to bool

--- statline_bq/utils.py
+++ statline_bq/utils.py
@@ -43,7 +43,7 @@ def check_gcp_env(gcp_env: str, options: List[str] = ["dev", "test", "prod"]) ->
         return True
 
 
-def check_v4(id: str, third_party: str = "False") -> str:
+def check_v4(id: str, third_party: bool = False) -> str:
     """Checks whether a certain CBS table exists as OData Version "v4".
 
     Parameters
@@ -51,7 +51,7 @@ def check_v4(id: str, third_party: str = "False") -> str:
     id: str
         CBS Dataset id, i.e. "83583NED"
 
-    third_party: str, default="False"
+    third_party: bool, default=False
         Flag to indicate dataset is not originally from CBS. Set to true
         to use dataderden.cbs.nl as base url (not available in v4 yet).
 
@@ -62,8 +62,8 @@ def check_v4(id: str, third_party: str = "False") -> str:
     """
 
     base_url = {
-        "True": None,  # currently no IV3 links in ODATA V4,
-        "False": f"https://odata4.cbs.nl/CBS/{id}",
+        True: None,  # currently no IV3 links in ODATA V4,
+        False: f"https://odata4.cbs.nl/CBS/{id}",
     }
     r = requests.get(base_url[third_party])
     if (
@@ -941,7 +941,7 @@ def get_col_descs_from_gcs(
 def cbsodata_to_gbq(
     id: str,
     odata_version: str,
-    third_party: str = "False",
+    third_party: str = False,
     source: str = "cbs",
     config: Config = None,
     gcp_env: str = None,
@@ -962,7 +962,7 @@ def cbsodata_to_gbq(
     odata_version: str
         version of the odata for this dataset - must be either "v3" or "v4".
 
-    third_party: str, default="False"
+    third_party: bool, default=False
         Flag to indicate dataset is not originally from CBS. Set to true
         to use dataderden.cbs.nl as base url (not available in v4 yet).
 
@@ -1153,7 +1153,7 @@ def cbsodata_to_gbq(
     return files_parquet  # TODO: return bq job ids
 
 
-def get_urls(id: str, odata_version: str, third_party: str = "False") -> dict:
+def get_urls(id: str, odata_version: str, third_party: bool = False) -> dict:
     """Returns a dict with urls of all dataset tables given a valid CBS dataset id.
 
     Parameters
@@ -1164,7 +1164,7 @@ def get_urls(id: str, odata_version: str, third_party: str = "False") -> dict:
     odata_version: str
         version of the odata for this dataset - must be either "v3" or "v4".
 
-    third_party: str, default="False"
+    third_party: bool, default=False
         Flag to indicate dataset is not originally from CBS. Set to true
         to use dataderden.cbs.nl as base url (not available in v4 yet).
 
@@ -1174,7 +1174,7 @@ def get_urls(id: str, odata_version: str, third_party: str = "False") -> dict:
 
     Examples:
     >>> dataset_id = '83583NED'
-    >>> urls = get_urls(id=dataset_id, odata_version="v3", third_party="False")
+    >>> urls = get_urls(id=dataset_id, odata_version="v3", third_party=False)
     >>> for name, url in urls.items():
     ...     print(f"{name}: {url}")
     TableInfos: https://opendata.cbs.nl/ODataFeed/odata/83583NED/TableInfos
@@ -1189,8 +1189,8 @@ def get_urls(id: str, odata_version: str, third_party: str = "False") -> dict:
 
     if odata_version == "v4":
         base_url = {
-            "True": None,  # currently no IV3 links in ODATA V4,
-            "False": f"https://odata4.cbs.nl/CBS/{id}",
+            True: None,  # currently no IV3 links in ODATA V4,
+            False: f"https://odata4.cbs.nl/CBS/{id}",
         }
         urls = {
             item["name"]: base_url[third_party] + "/" + item["url"]
@@ -1198,8 +1198,8 @@ def get_urls(id: str, odata_version: str, third_party: str = "False") -> dict:
         }
     elif odata_version == "v3":
         base_url = {
-            "True": f"https://dataderden.cbs.nl/ODataFeed/odata/{id}?$format=json",
-            "False": f"https://opendata.cbs.nl/ODataFeed/odata/{id}?$format=json",
+            True: f"https://dataderden.cbs.nl/ODataFeed/odata/{id}?$format=json",
+            False: f"https://opendata.cbs.nl/ODataFeed/odata/{id}?$format=json",
         }
         urls = {
             item["name"]: item["url"]
@@ -1451,7 +1451,7 @@ def gcs_to_gbq(
     id: str,
     source: str = "cbs",
     odata_version: str = None,
-    third_party: str = "False",
+    third_party: bool = False,
     config: Config = None,
     gcs_folder: str = None,
     file_names: list = None,
@@ -1471,7 +1471,7 @@ def gcs_to_gbq(
         The source of the dataset. Currently only "cbs" is relevant.
     odata_version: str
         version of the odata for this dataset - must be either "v3" or "v4".
-    third_party : str, default="False"
+    third_party : bool, default=False
         Flag to indicate dataset is not originally from CBS. Set to true to use dataderden.cbs.nl as base url (not available in v4 yet).
     config : Config object
         Config object holding GCP and local paths.
@@ -1567,7 +1567,7 @@ def gcs_to_gbq(
 def main(
     id: str,
     source: str = "cbs",
-    third_party: str = "False",
+    third_party: bool = False,
     config: Config = None,
     gcp_env: str = "dev",
     force: bool = False,
